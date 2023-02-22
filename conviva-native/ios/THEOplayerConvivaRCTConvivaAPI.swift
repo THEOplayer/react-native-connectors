e0a1f7995885134796c17ccf99e802493426829d
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2023-02-22 14:33:35 +0100

    Implement destroy and custom metadata

--- conviva-native/ios/THEOplayerConvivaRCTConvivaAPI.swift
+++ conviva-native/ios/THEOplayerConvivaRCTConvivaAPI.swift
@@ -9,7 +9,7 @@ import THEOplayerSDK
 class THEOplayerConvivaRCTConvivaAPI: NSObject, RCTBridgeModule {
     @objc var bridge: RCTBridge!
     
-    var connectors = [ConvivaConnector]()
+    var connectors = [NSNumber: ConvivaConnector]()
     
     static func moduleName() -> String! {
         return "ConvivaModule"
@@ -36,7 +36,7 @@ class THEOplayerConvivaRCTConvivaAPI: NSObject, RCTBridgeModule {
                     configuration: configuration,
                     player: player
                 ) {
-                    self.connectors.append(connector)
+                    self.connectors[node] = connector
                     print("[ConvivaModule] added connector to view", node)
                 }
             }
@@ -46,16 +46,29 @@ class THEOplayerConvivaRCTConvivaAPI: NSObject, RCTBridgeModule {
     @objc(setContentInfo:metadata:)
     func setContentInfo(_ node: NSNumber, metadata: NSDictionary) -> Void {
         print("[ConvivaModule] setContentInfo triggered.")
+        DispatchQueue.main.async {
+            if let connector = self.connectors[node], let contentInfo = metadata as? [String: Any] {
+                connector.videoAnalytics.setContentInfo(contentInfo)
+            }
+        }
     }
     
     @objc(setAdInfo:metadata:)
     func setAdInfo(_ node: NSNumber, metadata: NSDictionary) -> Void {
         print("[ConvivaModule] setAdInfo triggered.")
+        DispatchQueue.main.async {
+            if let connector = self.connectors[node], let adInfo = metadata as? [String: Any] {
+                connector.adAnalytics.setAdInfo(adInfo)
+            }
+        }
     }
     
     @objc(destroy:)
     func destroy(_ node: NSNumber) -> Void {
         print("[ConvivaModule] destroy triggered.")
+        DispatchQueue.main.async {
+            self.connectors.removeValue(forKey: node)
+        }
     }
     
 }
