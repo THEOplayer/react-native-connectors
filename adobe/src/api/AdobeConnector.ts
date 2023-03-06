4ed92f31c0bce9900bc6b476f123c3f8721b6f4b
Jeroen Veltmans <jeroen.veltmans@theoplayer.com>
2023-03-06 14:35:49 +0100

    Revert adobe project to initial setup

--- adobe/src/api/AdobeConnector.ts
+++ adobe/src/api/AdobeConnector.ts
@@ -1,10 +1,10 @@
-// import {ACPMedia} from '@adobe/react-native-acpmedia';
 import type { THEOplayer } from "react-native-theoplayer";
-// @ts-ignore
-import { ACPCore } from '@adobe/react-native-acpcore';
 
 export class AdobeConnector {
-  constructor(_player: THEOplayer) {
-    ACPCore.extensionVersion().then((version: string) => console.log("AdobeExperienceSDK: ACPCore version: " + version));
+  private player: THEOplayer;
+
+  constructor(player: THEOplayer) {
+    this.player = player
   }
+
 }
