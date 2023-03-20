7ea3a8b58595054b94a28669dc05547431d7c2b4
Wonne Joosen <wonne.joosen@theoplayer.com>
2023-03-20 14:16:22 +0100

    Add comscore configuration types

--- comscore/src/api/ComscoreConfiguration.ts
+++ comscore/src/api/ComscoreConfiguration.ts
@@ -1,3 +1,15 @@
-export interface ComscoreConfiguration {
-  // TODO
+export enum ComscoreUserConsent {
+  denied = "0",
+  granted = "1",
+  unknown = "-1"
 }
+
+export interface ComscoreConfiguration {
+  publisherId: string;
+  applicationName: string;
+  userConsent: ComscoreUserConsent;
+  debug?: boolean;
+}
+
+
+
