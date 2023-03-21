84a1c7d438c94e222845d074b33d6530fbaf8bc0
Wonne Joosen <wonne.joosen@theoplayer.com>
2023-03-21 15:19:02 +0100

    Remove comments and unused imports

--- comscore/src/internal/ComscoreConnectorAdapter.web.ts
+++ comscore/src/internal/ComscoreConnectorAdapter.web.ts
@@ -4,25 +4,13 @@ import type { ComscoreMetadata } from '../api/ComscoreMetadata';
 
 
 // @ts-ignore
-import type { ChromelessPlayer } from 'theoplayer';
 import { ComscoreTheo } from './ComscoreTheo';
 import { AdMetadata } from './ComscoreAdMetadata';
 import { ContentMetadata } from './ComscoreContentMetadata';
-import type { IContentMetadata } from './IComscoreContentMetadata';
 
 export class ComscoreConnectorAdapter {
 
   private integration: ComscoreTheo;
-
-  // // @ts-ignore
-  // constructor (player: THEOplayer, ComscoreMetadata: ComscoreMetadata, ComscoreConfig: ComscoreConfiguration) {
-  //   this.integration = new ComscoreTheo(
-  //     // @ts-ignore
-  //     player.nativeHandle as ChromelessPlayer,
-  //     ComscoreMetadata as NativeComscoreMetadata, // TODO uitleg vragen
-  //     ComscoreConfig
-  //   );
-  // }
 
   constructor (player: THEOplayer, ComscoreMetadata: ComscoreMetadata, ComscoreConfig: ComscoreConfiguration) {
     this.integration = new ComscoreTheo(
