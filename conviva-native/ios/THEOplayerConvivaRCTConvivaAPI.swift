b25fcdc744c83606cbdfb39bba0645dc67ce5bfc
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2023-03-30 17:37:26 +0200

    Only show logs in debug mode

--- conviva-native/ios/THEOplayerConvivaRCTConvivaAPI.swift
+++ conviva-native/ios/THEOplayerConvivaRCTConvivaAPI.swift
@@ -22,10 +22,10 @@ class THEOplayerConvivaRCTConvivaAPI: NSObject, RCTBridgeModule {
     
     @objc(initialize:convivaMetadata:convivaConfig:)
     func initialize(_ node: NSNumber, convivaMetadata: NSDictionary, convivaConfig: NSDictionary) -> Void {
-        print("[ConvivaModule] initialize triggered.")
+        log("initialize triggered.")
         
         DispatchQueue.main.async {
-            print("[ConvivaModule]", convivaConfig)
+            log(convivaConfig.debugDescription)
             if let player = self.player(for: node) {
                 let configuration = ConvivaConfiguration(
                     customerKey: convivaConfig["customerKey"] as! String,
@@ -37,24 +37,24 @@ class THEOplayerConvivaRCTConvivaAPI: NSObject, RCTBridgeModule {
                     player: player
                 ) {
                     self.connectors[node] = connector
-                    print("[ConvivaModule] added connector to view", node)
+                    log("added connector to view \(node)")
                     if let contentInfo = convivaMetadata as? [String: Any] {
                         connector.videoAnalytics.setContentInfo(contentInfo)
                     } else {
-                        print("[ConvivaModule] Received metada in wrong format. Received \(convivaMetadata), expected [String: Any]")
+                        log("Received metada in wrong format. Received \(convivaMetadata), expected [String: Any]")
                     }
                 } else {
-                    print("[ConvivaModule] Cannot create Conviva connector for node", node)
+                    log("Cannot create Conviva connector for node \(node)")
                 }
             } else {
-                print("[ConvivaModule] Cannot find THEOPlayer for node", node)
+                log("Cannot find THEOPlayer for node \(node)")
             }
         }
     }
     
     @objc(setContentInfo:metadata:)
     func setContentInfo(_ node: NSNumber, metadata: NSDictionary) -> Void {
-        print("[ConvivaModule] setContentInfo triggered.")
+        log("setContentInfo triggered.")
         DispatchQueue.main.async {
             if let connector = self.connectors[node], let contentInfo = metadata as? [String: Any] {
                 connector.videoAnalytics.setContentInfo(contentInfo)
@@ -64,7 +64,7 @@ class THEOplayerConvivaRCTConvivaAPI: NSObject, RCTBridgeModule {
     
     @objc(setAdInfo:metadata:)
     func setAdInfo(_ node: NSNumber, metadata: NSDictionary) -> Void {
-        print("[ConvivaModule] setAdInfo triggered.")
+        log("setAdInfo triggered.")
         DispatchQueue.main.async {
             if let connector = self.connectors[node], let adInfo = metadata as? [String: Any] {
                 connector.adAnalytics.setAdInfo(adInfo)
@@ -85,7 +85,7 @@ class THEOplayerConvivaRCTConvivaAPI: NSObject, RCTBridgeModule {
     
     @objc(destroy:)
     func destroy(_ node: NSNumber) -> Void {
-        print("[ConvivaModule] destroy triggered.")
+        log("destroy triggered.")
         DispatchQueue.main.async {
             self.connectors.removeValue(forKey: node)
         }
@@ -95,5 +95,10 @@ class THEOplayerConvivaRCTConvivaAPI: NSObject, RCTBridgeModule {
         let view = self.bridge.uiManager.view(forReactTag: node) as? THEOplayerRCTView
         return view?.player
     }
-    
+}
+
+func log(_ text: String) {
+    #if DEBUG
+        print("[react-native-theoplayer-conviva bridge]", text)
+    #endif
 }
