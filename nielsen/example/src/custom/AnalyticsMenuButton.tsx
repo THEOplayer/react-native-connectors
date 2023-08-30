8ad9005a9ba3ca5761c7ad40d67d502dcc61635d
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2023-08-30 16:08:30 +0200

    Update menu button

--- nielsen/example/src/custom/AnalyticsMenuButton.tsx
+++ nielsen/example/src/custom/AnalyticsMenuButton.tsx
@@ -7,28 +7,25 @@ export interface AnalyticsMenuOption {
   action: () => void;
 }
 
-interface AnalyticsMenuViewProps {
+interface AnalyticsMenuProps {
+  menuTitle: string;
   options: AnalyticsMenuOption[];
 }
 
-export const AnalyticsMenuButton = (props: AnalyticsMenuViewProps) => {
+export const AnalyticsMenuButton = (props: AnalyticsMenuProps) => {
   const createMenu = () => {
-    const { options } = props;
-    return <AnalyticsMenuView options={options} />;
+    const { menuTitle, options } = props;
+    return <AnalyticsMenuView menuTitle={menuTitle} options={options} />;
   };
   return <MenuButton svg={<AnalyticsSvg />} menuConstructor={createMenu} style={{justifyContent: 'center'}} />;
 };
 
-interface AnalyticsMenuButtonProps {
-  options: AnalyticsMenuOption[];
-}
-
-export const AnalyticsMenuView = (props: AnalyticsMenuButtonProps) => {
+export const AnalyticsMenuView = (props: AnalyticsMenuProps) => {
   return (
     <MenuView
       menu={
         <ScrollableMenu
-          title={'Nielsen Analytics Connector'}
+          title={props.menuTitle}
           items={props.options.map((option, id) => (
             <MenuRadioButton key={id} label={option.title} uid={id} onSelect={option.action}></MenuRadioButton>
           ))}
