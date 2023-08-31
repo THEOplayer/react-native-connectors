1e8eef24a8d6da00269d93c0a7e4953f89e35ff5
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2023-08-31 10:46:07 +0200

    Use UI in conviva example

--- conviva/example/src/res/images/index.ts
+++
@@ -1,6 +0,0 @@
-import PlayButton from './play.png';
-import PauseButton from './pause.png';
-import RewindButton from './rewind.png';
-import ForwardButton from './forward.png';
-import PipButton from './pip.png';
-export { PlayButton, PauseButton, RewindButton, ForwardButton };
