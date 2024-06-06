cd5200b0adfaf4bc5b8a35fae5e2119f78db6493
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-06-06 13:09:36 +0200

    Remove example apps

--- comscore/example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  TheoplayerComscoreExample
-//
-
-import Foundation
--- conviva/example/src/custom/Source.ts
+++
@@ -1,7 +0,0 @@
-import type { SourceDescription } from 'react-native-theoplayer';
-
-export interface Source {
-  name: string;
-  os: string[];
-  source: SourceDescription;
-}
