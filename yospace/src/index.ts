b07d09b44956ec8fe62689f5a2b15109ec859e0f
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-06-13 15:48:41 +0200

    Add yospace source types

--- yospace/src/index.ts
+++ yospace/src/index.ts
@@ -1,2 +1,4 @@
-export { YospaceConnector } from './api/YospaceConnector';
+export * from './api/YospaceConnector';
+export * from './api/YospaceSessionProperties';
+export * from './api/YospaceSSAIConfiguration';
 export { useYospace } from './api/hooks/useYospace';
