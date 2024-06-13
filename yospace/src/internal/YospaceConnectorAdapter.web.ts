aa8db32fd36fa17bf7bb708ede9d1e2f8d33a877
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-06-13 13:48:02 +0200

    Add support for web

--- yospace/src/internal/YospaceConnectorAdapter.web.ts
+++ yospace/src/internal/YospaceConnectorAdapter.web.ts
@@ -1,11 +1,15 @@
 import type { THEOplayer } from 'react-native-theoplayer';
+import { YospaceConnector } from "@theoplayer/yospace-connector-web";
+import type { ChromelessPlayer } from "theoplayer";
 
 export class YospaceConnectorAdapter {
+  private connector: YospaceConnector;
 
   constructor(player: THEOplayer) {
-    // TODO
+    this.connector = new YospaceConnector(player.nativeHandle as ChromelessPlayer);
   }
 
   destroy() {
+    /* unused */
   }
 }
