4b562c8fb1f8e323ae0dd0815cb0c09e6ccf2434
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-10-25 16:11:24 +0200

    Replace NativeModules

--- adobe-edge/src/utils/Utils.ts
+++ adobe-edge/src/utils/Utils.ts
@@ -1,5 +1,5 @@
 import type { Ad, AdBreak, TextTrackCue } from 'react-native-theoplayer';
-import { NativeModules, Platform } from 'react-native';
+import { I18nManager, Platform, Settings } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 import type { AdobeAdvertisingDetails, AdobeAdvertisingPodDetails, AdobeChapterDetails } from '@theoplayer/react-native-analytics-adobe-edge';
 
@@ -69,7 +69,7 @@ function nonEmptyOrUnknown(str?: string): string {
 export function buildUserAgent(): string {
   if (Platform.OS === 'android') {
     const { Release, Model: deviceName } = Platform.constants;
-    const localeString = nonEmptyOrUnknown(NativeModules.I18nManager?.localeIdentifier?.replace('_', '-'));
+    const localeString = nonEmptyOrUnknown(I18nManager?.getConstants()?.localeIdentifier?.replace('_', '-'));
     const operatingSystem = `Android ${Release}`;
     const deviceBuildId = nonEmptyOrUnknown(DeviceInfo.getBuildIdSync());
     // operatingSystem: `Android Build.VERSION.RELEASE`
@@ -77,7 +77,7 @@ export function buildUserAgent(): string {
     // Example: Mozilla/5.0 (Linux; U; Android 7.1.2; en-US; AFTN Build/NS6296)
     return `${USER_AGENT_PREFIX} (Linux; U; ${operatingSystem}; ${localeString}; ${deviceName} Build/${deviceBuildId})`;
   } else if (Platform.OS === 'ios') {
-    const localeString = NativeModules.SettingsManager.settings.AppleLocale || NativeModules.SettingsManager.settings.AppleLanguages[0];
+    const localeString = Settings.get('AppleLocale') || Settings.get('AppleLanguages')[0];
     const model = DeviceInfo.getModel();
     const osVersion = DeviceInfo.getSystemVersion().replace('.', '_');
     return `${USER_AGENT_PREFIX} (${model}; CPU OS ${osVersion} like Mac OS X; ${localeString})`;
