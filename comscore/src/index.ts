9f0f49ac13d2e60583e7a593634be29f3cea52fe
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-10-25 16:13:02 +0200

    Fix API export

--- comscore/src/index.ts
+++ comscore/src/index.ts
@@ -1,15 +1,4 @@
 export { ComscoreConnector } from './api/ComscoreConnector';
-export type { ComscoreConfiguration } from './api/ComscoreConfiguration';
-export { ComscoreUserConsent, ComscoreUsagePropertiesAutoUpdateMode } from './api/ComscoreConfiguration';
-export {
-  ComscoreMediaType,
-  ComscoreFeedType,
-  ComscoreDeliveryMode,
-  ComscoreDeliverySubscriptionType,
-  ComscoreDeliveryComposition,
-  ComscoreDeliveryAdvertisementCapability,
-  ComscoreMediaFormat,
-  ComscoreDistributionModel,
-} from './api/ComscoreMetadata';
-export type { ComscoreMetadata, CustomComscoreMetadata, ComscoreDate, ComscoreTime, ComscoreDimension } from './api/ComscoreMetadata';
+export * from './api/ComscoreConfiguration';
+export * from './api/ComscoreMetadata';
 export { useComscore } from './api/hooks/useComscore';
