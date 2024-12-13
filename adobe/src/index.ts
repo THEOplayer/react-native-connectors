2c630d14dca7dccac77a298ba63869665a58abe1
William Van Haevre <william.vanhaevre@gmail.com>
2024-12-13 17:42:46 +0100

    Add sdkVersions API to Adobe SDK.

--- adobe/src/index.ts
+++ adobe/src/index.ts
@@ -1,2 +1,3 @@
 export { AdobeConnector, type AdobeMetaData } from './api/AdobeConnector';
 export { useAdobe } from './api/hooks/useAdobe';
+export { sdkVersions } from './internal/version/Version';
