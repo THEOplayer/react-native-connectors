15611d44faaf29022101bd43ac3573d53a39c05a
William Van Haevre <william.vanhaevre@gmail.com>
2024-12-13 17:33:53 +0100

    Add sdkVersion prop to Engage SDK.

--- engage/src/index.ts
+++ engage/src/index.ts
@@ -2,3 +2,4 @@ export * from './api';
 export * from './internal/event/Event';
 export * from './internal/event/EventListener';
 export * from './internal/event/EventDispatcher';
+export { sdkVersion } from './internal/version/Version';
