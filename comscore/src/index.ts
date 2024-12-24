4e6906b3a8d4972328d7b91e073bf43abf2e77e1
William Van Haevre <william.vanhaevre@gmail.com>
2024-12-24 11:56:35 +0100

    Add sdkVersions API to Comscore SDK.

--- comscore/src/index.ts
+++ comscore/src/index.ts
@@ -2,3 +2,4 @@ export { ComscoreConnector } from './api/ComscoreConnector';
 export * from './api/ComscoreConfiguration';
 export * from './api/ComscoreMetadata';
 export { useComscore } from './api/hooks/useComscore';
+export { sdkVersions } from './internal/version/Version';
