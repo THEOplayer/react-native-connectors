87667f23199f535ba64dae92d50eeabb73f02375
William Van Haevre <william.vanhaevre@gmail.com>
2024-12-24 11:57:03 +0100

    Add sdkVersions API to Mux SDK.

--- mux/src/index.ts
+++ mux/src/index.ts
@@ -2,3 +2,4 @@ export { MuxConnector, MuxPresentation } from './api/MuxConnector';
 export type { MuxData } from './api/MuxData';
 export type { MuxOptions } from './api/MuxOptions';
 export { useMux } from './api/hooks/useMux';
+export { sdkVersions } from './internal/version/Version';
