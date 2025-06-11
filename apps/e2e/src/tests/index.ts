eb9e352f9e10d3da62461b26ac21ee6913c5ab65
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-06-11 14:57:59 +0200

    Add Yospace e2e test for Android

--- apps/e2e/src/tests/index.ts
+++ apps/e2e/src/tests/index.ts
@@ -2,5 +2,12 @@ import Adobe from './Adobe.spec';
 import Comscore from './Comscore.spec';
 import Conviva from './Conviva.spec';
 import Nielsen from './Nielsen.spec';
+import Yospace from './Yospace.spec';
+import { Platform } from 'react-native';
 
-export default [Adobe, Comscore, Conviva, Nielsen];
+const tests = [Adobe, Comscore, Conviva, Nielsen];
+if (Platform.OS === 'android') {
+  tests.push(Yospace);
+}
+
+export default tests;
