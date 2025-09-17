5748a4f61787d74f5b91109aad7b1b3ec863d7d4
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-09-17 13:31:21 +0200

    Add version support

--- adobe-edge/src/index.ts
+++ adobe-edge/src/index.ts
@@ -2,3 +2,4 @@ export { AdobeConnector } from './api/AdobeConnector';
 export * from './api/details/barrel';
 export { useAdobe } from './api/hooks/useAdobe';
 export { useAdobeNative } from './api/hooks/useAdobeNative';
+export { sdkVersions } from './internal/version/Version';
