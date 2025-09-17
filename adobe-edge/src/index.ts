0156eb6c5b8ceb946202d50356188b42b73d3835
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-09-17 13:29:33 +0200

    Use native convenience hooks

--- adobe-edge/src/index.ts
+++ adobe-edge/src/index.ts
@@ -1,3 +1,4 @@
 export { AdobeConnector } from './api/AdobeConnector';
 export * from './api/details/barrel';
 export { useAdobe } from './api/hooks/useAdobe';
+export { useAdobeNative } from './api/hooks/useAdobeNative';
