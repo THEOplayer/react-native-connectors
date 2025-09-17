f545e555036276b271ef4208adb18d94e547623b
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-09-17 12:01:25 +0200

    Add adobe tests

--- apps/e2e/src/tests/index.ts
+++ apps/e2e/src/tests/index.ts
@@ -1,11 +1,13 @@
 import Adobe from './Adobe.spec';
+import AdobeNative from './AdobeNative.spec';
+import AdobeEdge from './AdobeEdge.spec';
 import Comscore from './Comscore.spec';
 import Conviva from './Conviva.spec';
 import Nielsen from './Nielsen.spec';
 import Yospace from './Yospace.spec';
 import { Platform } from 'react-native';
 
-const tests = [Adobe, Comscore, Conviva, Nielsen];
+const tests = [Adobe, AdobeNative, AdobeEdge, Comscore, Conviva, Nielsen];
 if (Platform.OS === 'android') {
   tests.push(Yospace);
 }
