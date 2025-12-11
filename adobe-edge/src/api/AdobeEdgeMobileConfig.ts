c92da989df187a66ac19604a0f699d14f663f04e
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-12-11 13:12:57 +0100

    Update docs

--- adobe-edge/src/api/AdobeEdgeMobileConfig.ts
+++ adobe-edge/src/api/AdobeEdgeMobileConfig.ts
@@ -1,5 +1,25 @@
 export interface AdobeEdgeMobileConfig {
+  /**
+   * The unique environment ID that the SDK uses to retrieve your configuration.
+   * This ID is generated when an app configuration is created and published to a given environment.
+   * The app is first launched and then the SDK retrieves and uses this Adobe-hosted configuration.
+   * {@link https://developer.adobe.com/client-sdks/previous-versions/documentation/mobile-core/configuration/}
+   */
   appId?: string;
 
+  /**
+   * On Android, instead of providing an `appId` that is used to retrieve the Adobe-hosted configuration,
+   * load the configuration from a JSON configuration file in the app's Assets folder.
+   *
+   * {@link https://developer.adobe.com/client-sdks/home/base/mobile-core/api-reference/#setloglevel}
+   * @platform android
+   */
+  configAsset?: string;
+
+  /**
+   * The debugEnabled property allows you to enable or disable debugging using SDK code.
+   *
+   * @default `false`.
+   */
   debugEnabled?: boolean;
 }
