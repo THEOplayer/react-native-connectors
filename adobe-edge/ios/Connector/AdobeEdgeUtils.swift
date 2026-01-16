83f82909c0c423c9b74b3c27259246ab4dbf0037
William Van Haevre <william.vanhaevre@gmail.com>
2026-01-16 12:16:49 +0100

    Drop unused method

--- adobe-edge/ios/Connector/AdobeEdgeUtils.swift
+++ adobe-edge/ios/Connector/AdobeEdgeUtils.swift
@@ -5,17 +5,6 @@ import THEOplayerSDK
 import AEPEdgeIdentity
 
 class AdobeEdgeUtils {
-    class func toAdobeCustomMetadataDetails(_ array: [[String: Any]]) -> [String: String] {
-        var result = [String: String]()
-        for item in array {
-            let stringsItem = AdobeEdgeUtils.toStringMap(item)
-            if let name = stringsItem["name"], let value = stringsItem["value"] {
-                result[name] = value
-            }
-        }
-        return result
-    }
-    
     class func toStringMap(_ map: [String: Any]) -> [String: String] {
         var result = [String: String]()
         for (key, value) in map {
