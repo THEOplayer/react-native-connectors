6d095f9d7a2d650e332e1a54a5361ef08f719570
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2026-01-16 10:36:39 +0100

    Fix exports

--- adobe-edge/src/api/details/barrel.ts
+++ adobe-edge/src/api/details/barrel.ts
@@ -1,3 +1,3 @@
-export type { AdobeIdentityMap } from './AdobeIdentityMap';
-export type { AdobeIdentityItem } from './AdobeIdentityItem';
-export type { AdobeMetadata } from './AdobeMetadata';
+export * from './AdobeIdentityMap';
+export * from './AdobeIdentityItem';
+export * from './AdobeMetadata';
--- adobe-edge/src/index.ts
+++ adobe-edge/src/index.ts
@@ -1,7 +1,7 @@
 export { AdobeConnector } from './api/AdobeConnector';
 export type { AdobeEdgeConfig } from './api/AdobeEdgeConfig';
-export type { AdobeEdgeMobileConfig } from './api/AdobeEdgeMobileConfig';
-export type { AdobeEdgeWebConfig } from './api/AdobeEdgeWebConfig';
+export * from './api/AdobeEdgeMobileConfig';
+export * from './api/AdobeEdgeWebConfig';
 export * from './api/details/barrel';
 export { useAdobe } from './api/hooks/useAdobe';
 export { sdkVersions } from './internal/version/Version';
