e7e3c66da4236b01d9678bc325065512d537a490
William Van Haevre <william.vanhaevre@gmail.com>
2026-01-26 15:08:43 +0100

    Connect bridged methods to BitmovinConnector functionality

--- bitmovin/ios/THEOplayerBitmovinRCTBitmovinAPI.swift
+++ bitmovin/ios/THEOplayerBitmovinRCTBitmovinAPI.swift
@@ -5,48 +5,79 @@ import react_native_theoplayer
 import THEOplayerSDK
 
 func log(_ text: String) {
-    #if DEBUG
-        print("[react-native-theoplayer-bitmovin]", text)
-    #endif
+#if DEBUG
+    print("[react-native-theoplayer-bitmovin]", text)
+#endif
 }
 
 @objc(THEOplayerBitmovinRCTBitmovinAPI)
 class THEOplayerBitmovinRCTBitmovinAPI: NSObject, RCTBridgeModule {
     @objc var bridge: RCTBridge!
-
-//     var connectors = [NSNumber: BitmovinConnector]()
-
+    
+    var connectors = [NSNumber: BitmovinConnector]()
+    
     static func moduleName() -> String! {
         return "BitmovinModule"
     }
-
+    
     static func requiresMainQueueSetup() -> Bool {
         return false
     }
-
-    @objc(initialize:bitmovinOptions:)
-    func initialize(_ node: NSNumber, bitmovinConfig: NSDictionary) -> Void {
+    
+    @objc(initialize:bitmovinConfig:defaultMetadata:)
+    func initialize(_ node: NSNumber, bitmovinConfig: NSDictionary, defaultMetadata: NSDictionary?) -> Void {
         log("initialize triggered.")
-
-        /*DispatchQueue.main.async {
-            log("\(bitmovinConfig)")
+        
+        DispatchQueue.main.async {
             let theView = self.bridge.uiManager.view(forReactTag: node) as? THEOplayerRCTView
             if let player = theView?.player,
-			      var options = bitmovinConfig as? [String:Any] {
+               var config = bitmovinConfig as? [String:Any] {
+                let metadata = defaultMetadata as? [String:Any]
+                let connector = BitmovinConnector(player: player, bitmovinConfig: config, defaultMetadata: metadata)
+                self.connectors[node] = connector
+                log("added connector to view \(node)")
+            } else {
+                log("cannot find THEOPlayer for node \(node)")
             }
-        }*/
+        }
     }
-
-    @objc(updateMetadata:metadata:)
-    func updateMetadata(for node: NSNumber, metadata: NSDictionary) {
-        log("Warning: updating metadata not possible on iOS.")
+    
+    @objc(updateSourceMetadata:metadata:)
+    func updateSourceMetadata(_ node: NSNumber, metadata: NSDictionary) {
+        log("updateSourceMetadata triggered.")
+        DispatchQueue.main.async {
+            if let sourceMetadata = metadata as? [String:Any] {
+                self.connectors[node]?.updateSourceMetadata(sourceMetadata)
+            }
+        }
     }
-
+    
+    @objc(updateCustomMetadata:customData:)
+    func updateCustomMetadata(_ node: NSNumber, customData: NSDictionary) {
+        log("updateCustomMetadata triggered.")
+        DispatchQueue.main.async {
+            if let customMetadata = customData as? [String:Any] {
+                self.connectors[node]?.updateCustomMetadata(customMetadata)
+            }
+        }
+    }
+    
+    @objc(sendCustomDataEvent:customData:)
+    func sendCustomDataEvent(_ node: NSNumber, customData: NSDictionary) {
+        log("sendCustomDataEvent triggered.")
+        DispatchQueue.main.async {
+            if let customMetadata = customData as? [String:Any] {
+                self.connectors[node]?.sendCustomDataEvent(customMetadata)
+            }
+        }
+    }
+    
     @objc(destroy:)
     func destroy(_ node: NSNumber) -> Void {
-      log("destroy triggered for \(node).")
-//       DispatchQueue.main.async {
-//           self.connectors.removeValue(forKey: node)
-//       }
+        log("destroy triggered for \(node).")
+        DispatchQueue.main.async {
+            self.connectors[node]?.destroy()
+            self.connectors.removeValue(forKey: node)
+        }
     }
 }
