c3681d9c72dcb016b3bc0bb933353b8ff2b47681
William Van Haevre <william.vanhaevre@gmail.com>
2026-01-26 15:08:13 +0100

    Update objc bridging code

--- bitmovin/ios/THEOplayerBitmovinRCTBridge.m
+++ bitmovin/ios/THEOplayerBitmovinRCTBridge.m
@@ -8,9 +8,14 @@
 @interface RCT_EXTERN_REMAP_MODULE(BitmovinModule, THEOplayerBitmovinRCTBitmovinAPI, NSObject)
 
 RCT_EXTERN_METHOD(initialize:(nonnull NSNumber *)node
-                  bitmovinConfig:(NSDictionary)bitmovinConfig)
+                  bitmovinConfig:(nonnull NSDictionary *)bitmovinConfig
+                  defaultMetadata:(nullable NSDictionary *)defaultMetadata)
 
-RCT_EXTERN_METHOD(updateMetadata:(nonnull NSNumber *)node metadata:(nonnull NSDictionary *)metadata)
+RCT_EXTERN_METHOD(updateSourceMetadata:(nonnull NSNumber *)node metadata:(nonnull NSDictionary *)metadata)
+
+RCT_EXTERN_METHOD(updateCustomMetadata:(nonnull NSNumber *)node customData:(nonnull NSDictionary *)customData)
+
+RCT_EXTERN_METHOD(sendCustomDataEvent:(nonnull NSNumber *)node customData:(nonnull NSDictionary *)customData)
 
 RCT_EXTERN_METHOD(destroy:(nonnull NSNumber *)node)
 
