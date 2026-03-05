6bd2f26b9062595d8c18afec430b532ea4f98bd1
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2026-03-05 09:18:42 +0100

    Fix android tests

--- apps/e2e/android/app/src/main/java/com/reactnativetheoplayer/MainApplication.kt
+++ apps/e2e/android/app/src/main/java/com/reactnativetheoplayer/MainApplication.kt
@@ -12,6 +12,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 import com.theoplayer.reactnative.adobe.edge.ReactTHEOplayerAdobeEdgePackage
+import com.theoplayer.reactnative.adobe.ReactTHEOplayerAdobePackage
 import com.theoplayer.reactnative.bitmovin.ReactTHEOplayerBitmovinPackage
 import com.theoplayercomscore.ReactTHEOplayerComscorePackage
 import com.theoplayerconviva.ReactTHEOplayerConvivaPackage
@@ -28,6 +29,7 @@ class MainApplication : Application(), ReactApplication {
           add(ReactTHEOplayerComscorePackage())
           add(ReactTHEOplayerConvivaPackage())
           add(ReactTHEOplayerNielsenPackage())
+          add(ReactTHEOplayerAdobePackage())
           add(ReactTHEOplayerAdobeEdgePackage())
 //          add(ReactTHEOplayerYospacePackage())
         }
