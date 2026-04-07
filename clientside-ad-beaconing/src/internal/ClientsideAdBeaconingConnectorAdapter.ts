2a27eb6c9fc6b392f9df54b0acf9093565498998
William Van Haevre <william.vanhaevre@gmail.com>
2026-04-07 18:00:36 +0200

    Setup basic adapter interface

--- clientside-ad-beaconing/src/internal/ClientsideAdBeaconingConnectorAdapter.ts
+++ clientside-ad-beaconing/src/internal/ClientsideAdBeaconingConnectorAdapter.ts
@@ -1,5 +1,6 @@
 import type { NativeHandleType, THEOplayer } from 'react-native-theoplayer';
 import { NativeModules } from 'react-native';
+import { AdBreakInit, AdInit } from 'theoplayer';
 
 const TAG = 'ClientsideAdBeaconingConnector';
 const ERROR_MSG = 'ClientsideAdBeaconingConnectorAdapter Error';
@@ -11,6 +12,54 @@ export class ClientsideAdBeaconingConnectorAdapter {
     try {
       this.nativeHandle = player.nativeHandle || -1;
       NativeModules.ClientsideAdBeaconingModule.initialize(this.nativeHandle);
+    } catch (error: unknown) {
+      console.error(TAG, `${ERROR_MSG}: ${error}`);
+    }
+  }
+
+  createAdBreak(adBreakId: string, adBreakInit: AdBreakInit): void {
+    try {
+      NativeModules.ClientsideAdBeaconingModule.createAdBreak(this.nativeHandle, adBreakId, adBreakInit);
+    } catch (error: unknown) {
+      console.error(TAG, `${ERROR_MSG}: ${error}`);
+    }
+  }
+
+  createAd(adId: string, adInit: AdInit): void {
+    try {
+      NativeModules.ClientsideAdBeaconingModule.createAdBreak(this.nativeHandle, adId, adInit);
+    } catch (error: unknown) {
+      console.error(TAG, `${ERROR_MSG}: ${error}`);
+    }
+  }
+
+  beginAd(adId: string): void {
+    try {
+      NativeModules.ClientsideAdBeaconingModule.beginAd(this.nativeHandle, adId);
+    } catch (error: unknown) {
+      console.error(TAG, `${ERROR_MSG}: ${error}`);
+    }
+  }
+
+  endAd(adId: string): void {
+    try {
+      NativeModules.ClientsideAdBeaconingModule.endAd(this.nativeHandle, adId);
+    } catch (error: unknown) {
+      console.error(TAG, `${ERROR_MSG}: ${error}`);
+    }
+  }
+
+  removeAd(adId: string): void {
+    try {
+      NativeModules.ClientsideAdBeaconingModule.removeAd(this.nativeHandle, adId);
+    } catch (error: unknown) {
+      console.error(TAG, `${ERROR_MSG}: ${error}`);
+    }
+  }
+
+  removeAdBreak(adBreakId: string): void {
+    try {
+      NativeModules.ClientsideAdBeaconingModule.removeAdBreak(this.nativeHandle, adBreakId);
     } catch (error: unknown) {
       console.error(TAG, `${ERROR_MSG}: ${error}`);
     }
--- clientside-ad-beaconing/src/internal/ClientsideAdBeaconingConnectorAdapter.web.ts
+++ clientside-ad-beaconing/src/internal/ClientsideAdBeaconingConnectorAdapter.web.ts
@@ -1,17 +1,36 @@
 import type { THEOplayer } from 'react-native-theoplayer';
-
-const CSAB_ANALYTICS_AUGMENTED_MARKER = '__clientsideadBeaconingHasBeenSetup';
+import { AdBreakInit, AdInit } from 'theoplayer';
 
 export class ClientsideAdBeaconingConnectorAdapter {
   constructor(player: THEOplayer) {
     console.log('ClientsideAdBeaconingConnectorAdapter constructor called');
   }
 
+  createAdBreak(adBreakId: string, adBreakInit: AdBreakInit): void {
+    console.log('ClientsideAdBeaconingConnectorAdapter createAdBreak called for adBreakId:', adBreakId, 'with adBreakInit:', adBreakInit);
+  }
+
+  createAd(adId: string, adInit: AdInit): void {
+    console.log('ClientsideAdBeaconingConnectorAdapter createAd called for adId:', adId, 'with adInit:', adInit);
+  }
+
+  beginAd(adId: string): void {
+    console.log('ClientsideAdBeaconingConnectorAdapter beginAd called for adId:', adId);
+  }
+
+  endAd(adId: string): void {
+    console.log('ClientsideAdBeaconingConnectorAdapter endAd called for adId:', adId);
+  }
+
+  removeAd(adId: string): void {
+    console.log('ClientsideAdBeaconingConnectorAdapter removeAd called for adId:', adId);
+  }
+
+  removeAdBreak(adBreakId: string): void {
+    console.log('ClientsideAdBeaconingConnectorAdapter removeAdBreak called for adBreakId:', adBreakId);
+  }
+
   destroy() {
     console.log('ClientsideAdBeaconingConnectorAdapter destroy called');
-    const container = document.querySelector('.theoplayer-container');
-    if (container) {
-      container[CSAB_ANALYTICS_AUGMENTED_MARKER] = false;
-    }
   }
 }
